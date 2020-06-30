abd115dba382183eb519346a0bee7631fb62c418
senti50 <46302409+senti50@users.noreply.github.com>
2020-07-01 00:48:20 +0200

    adding switching flash and focus to camera

--- app/src/main/java/com/example/myphotostock/CameraFragment.kt
+++ app/src/main/java/com/example/myphotostock/CameraFragment.kt
@@ -1,14 +1,16 @@
 package com.example.myphotostock
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.media.Image
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
@@ -17,17 +19,22 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.myphotostock.databinding.FragmentCameraBinding
 import com.google.common.util.concurrent.ListenableFuture
+import kotlinx.android.synthetic.main.activity_image_preview.*
+import kotlinx.android.synthetic.main.fragment_camera.*
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import kotlin.math.absoluteValue
 
 
 private const val REQUEST_CODE_PERMISSIONS = 1
 
 private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
 class CameraFragment : Fragment() {
-    companion object{
+    companion object {
         private const val FILENAME = "yyyy-MM-dd-HH-mm-ss-SSS"
         private const val PHOTO_EXTENSION = ".jpg"
 
@@ -36,30 +43,36 @@ class CameraFragment : Fragment() {
             val mediaDir = context.externalMediaDirs.firstOrNull()?.let {
                 File(it, appContext.resources.getString(R.string.app_name)).apply { mkdirs() }
             }
-            return if (mediaDir != null && mediaDir.exists())mediaDir else appContext.filesDir
+            return if (mediaDir != null && mediaDir.exists()) mediaDir else appContext.filesDir
         }
 
         fun createFile(baseFolder: File, format: String, extension: String) =
-            File(baseFolder, SimpleDateFormat(format, Locale.US)
-                .format(System.currentTimeMillis()) + extension)
+            File(
+                baseFolder, SimpleDateFormat(format, Locale.US)
+                    .format(System.currentTimeMillis()) + extension
+            )
     }
-    private var _binding: FragmentCameraBinding?=null
+
+    private var _binding: FragmentCameraBinding? = null
     private lateinit var imageCapture: ImageCapture
     private val binding get() = _binding!!
-    private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
     private lateinit var previewView: PreviewView
     private lateinit var imagePreview: Preview
     private val executor = Executors.newSingleThreadExecutor()
     private lateinit var outputDirectory: File
-    var imagePath=""
+    var imagePath = ""
+    private lateinit var cameraControl: CameraControl
+    private lateinit var cameraInfo: CameraInfo
+    private var flashMode: Int = ImageCapture.FLASH_MODE_OFF
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 //        // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_camera, container, false)
-        _binding=FragmentCameraBinding.inflate(inflater,container,false)
-        val view=binding.root
+        _binding = FragmentCameraBinding.inflate(inflater, container, false)
+        val view = binding.root
         previewView = binding.previewView
 
         cameraProviderFuture = ProcessCameraProvider.getInstance(context!!)
@@ -73,24 +86,51 @@ class CameraFragment : Fragment() {
         }
         outputDirectory = getOutputDirectory(requireContext())
         binding.cameraCaptureButton.setOnClickListener {
-            takePicture()
-            previewView.post {
-                Toast.makeText(context, imagePath, Toast.LENGTH_LONG).show()
+            when (flashMode) {
+                ImageCapture.FLASH_MODE_OFF ->
+                    cameraControl.enableTorch(false)
+
+
+                ImageCapture.FLASH_MODE_ON ->
+                    cameraControl.enableTorch(true)
             }
-            Log.i("FRAGMENT",imagePath)
-            activity?.let{
-                val intent = Intent (activity, ImagePreviewActivity::class.java)
-                intent.putExtra("imagePath",imagePath)
+            takePicture()
+            Handler().postDelayed({activity?.let {
+                val intent = Intent(activity, ImagePreviewActivity::class.java)
+                intent.putExtra("imagePath", imagePath)
                 startActivity(intent)
 
-            }
+            }},1000)
+            Log.i("FRAGMENT", imagePath)
+
 
         }
+        binding.cameraTorchButton.setOnClickListener {
+            when (flashMode) {
+                ImageCapture.FLASH_MODE_OFF ->{
+                    flashMode = ImageCapture.FLASH_MODE_ON
+                    binding.cameraTorchButton.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            requireContext(),
+                            R.drawable.ic_flash_on_24dp
+                        )
+                    )
+                }
+                ImageCapture.FLASH_MODE_ON ->{
+                    flashMode = ImageCapture.FLASH_MODE_OFF
+                    binding.cameraTorchButton.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            requireContext(),
+                            R.drawable.ic_flash_off_24dp
+                        ))
+                }else->flashMode=ImageCapture.FLASH_MODE_OFF
+            }
+        }
+
 
 
         return view
     }
-
 
     private fun takePicture() {
         val file = createFile(
@@ -100,27 +140,37 @@ class CameraFragment : Fragment() {
         )
 
         val outputFileOptions = ImageCapture.OutputFileOptions.Builder(file).build()
-        imageCapture.takePicture(outputFileOptions, executor, object : ImageCapture.OnImageSavedCallback {
-            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+        imageCapture.takePicture(
+            outputFileOptions,
+            executor,
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    previewView.post {
+                        Toast.makeText(context, "Photo capture succeeded: ${file.absolutePath}", Toast.LENGTH_LONG).show()
+                    }
 
-
-            }
-
-            override fun onError(exception: ImageCaptureException) {
-                val msg = "Photo capture failed: ${exception.message}"
-                Log.e("CameraFragment", "Photo capture failed: ${exception.message}", exception)
-                previewView.post {
-                    Toast.makeText(context, msg, Toast.LENGTH_LONG).show()
                 }
-            }
-        })
-        imagePath=file.absolutePath
+
+                override fun onError(exception: ImageCaptureException) {
+                    val msg = "Photo capture failed: ${exception.message}"
+                    Log.e("CameraFragment", "Photo capture failed: ${exception.message}", exception)
+                    previewView.post {
+                        Toast.makeText(context, msg, Toast.LENGTH_LONG).show()
+                    }
+                }
+            })
+        imagePath = file.absolutePath
 
     }
+
     override fun onDestroy() {
         super.onDestroy()
-        _binding=null
+        _binding = null
     }
+
+
+
+    @SuppressLint("ClickableViewAccessibility")
     private fun startCamera() {
 
         imagePreview = Preview.Builder().apply {
@@ -128,17 +178,93 @@ class CameraFragment : Fragment() {
             setTargetRotation(previewView.display.rotation)
         }.build()
         imageCapture = ImageCapture.Builder().apply {
-           setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+            setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .setFlashMode(flashMode)
         }.build()
-        val cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+        val cameraSelector =
+            CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
         cameraProviderFuture.addListener(Runnable {
             val cameraProvider = cameraProviderFuture.get()
-            cameraProvider.bindToLifecycle(this, cameraSelector, imagePreview, imageCapture)
+            val camera =
+                cameraProvider.bindToLifecycle(this, cameraSelector, imagePreview, imageCapture)
+            cameraControl = camera.cameraControl
+            cameraInfo = camera.cameraInfo
+            //Auto-focus every X seconds
+            previewView.afterMeasured {
+                val factory: MeteringPointFactory = SurfaceOrientedMeteringPointFactory(
+                    previewView.width.toFloat(), previewView.height.toFloat()
+                )
+                val centerWidth = previewView.width.toFloat() / 2
+                val centerHeight = previewView.height.toFloat() / 2
+                //create a point on the center of the view
+                val autoFocusPoint = factory.createPoint(centerWidth, centerHeight)
+                try {
+                    camera.cameraControl.startFocusAndMetering(
+                        FocusMeteringAction.Builder(
+                            autoFocusPoint,
+                            FocusMeteringAction.FLAG_AF
+                        ).apply {
+                            //auto-focus every 1 seconds
+                            setAutoCancelDuration(1, TimeUnit.SECONDS)
+                        }.build()
+                    )
+                } catch (e: CameraInfoUnavailableException) {
+                    Log.d("ERROR", "cannot access camera", e)
+                }
+            }
+            //Focus on-tap
+            previewView.afterMeasured {
+                previewView.setOnTouchListener { _, event ->
+                    return@setOnTouchListener when (event.action) {
+                        MotionEvent.ACTION_DOWN -> {
+                            true
+                        }
+                        MotionEvent.ACTION_UP -> {
+                            val factory: MeteringPointFactory = SurfaceOrientedMeteringPointFactory(
+                                previewView.width.toFloat(), previewView.height.toFloat()
+                            )
+                            val autoFocusPoint = factory.createPoint(event.x, event.y)
+                            try {
+                                camera.cameraControl.startFocusAndMetering(
+                                    FocusMeteringAction.Builder(
+                                        autoFocusPoint,
+                                        FocusMeteringAction.FLAG_AF
+                                    ).apply {
+                                        //focus only when the user tap the preview
+                                        disableAutoCancel()
+                                    }.build()
+                                )
+                            } catch (e: CameraInfoUnavailableException) {
+                                Log.d("ERROR", "cannot access camera", e)
+                            }
+                            true
+                        }
+                        else -> false // Unhandled event.
+                    }
+                }
+            }
             previewView.preferredImplementationMode = PreviewView.ImplementationMode.TEXTURE_VIEW
             imagePreview.setSurfaceProvider(previewView.createSurfaceProvider())
         }, ContextCompat.getMainExecutor(context))
     }
+
+    private inline fun View.afterMeasured(crossinline block: () -> Unit) {
+        viewTreeObserver.addOnGlobalLayoutListener(object :
+            ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                if (measuredWidth > 0 && measuredHeight > 0) {
+                    viewTreeObserver.removeOnGlobalLayoutListener(this)
+                    block()
+                }
+            }
+        })
+    }
+
+
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(context!!, it) == PackageManager.PERMISSION_GRANTED
     }
 }
+
+
+
